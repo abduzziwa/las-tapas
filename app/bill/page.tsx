9454d48de5d15ad90ba7245f447c844baf993640
Chris Kettmann <chpkettmann@gmail.com>
2024-09-12 14:02:29 +0200

    changes

--- app/bill/page.tsx
+++ app/bill/page.tsx
@@ -1,12 +1,14 @@
 import TopNavBar from "../components/TopNavBar"; // Import the TopNavBar component
-import BottomNavBar from "../components/BottomNavBar";   // Import the BottomNavBar component
+import BottomNavBar from "../components/BottomNavBar"; // Import the BottomNavBar component
 
 export default function Bill() {
-    return (
+  return (
     <main>
-        <TopNavBar />
-            <h1>Bill</h1>
-        <BottomNavBar />
+      <TopNavBar />
+      <div className="flex flex-col items-center align-middle w-full h-max text-[24px] leading-tight px-[14px] py-[24px] gap-[14px]">
+        <h1>Bill</h1>
+      </div>
+      <BottomNavBar />
     </main>
-);
+  );
 }
--- app/menu/foods/page.tsx
+++ app/menu/foods/page.tsx
@@ -1,12 +1,21 @@
 import TopNavBar from "../../components/TopNavBar"; // Import the TopNavBar component
-import BottomNavBar from "../../components/BottomNavBar";   // Import the BottomNavBar component
+import BottomNavBar from "../../components/BottomNavBar"; // Import the BottomNavBar component
+import Link from "next/link";
 
 export default function Foods() {
-    return (
+  return (
     <main>
-        <TopNavBar />
-            <h1>Foods</h1>
-        <BottomNavBar />
+      <TopNavBar />
+
+      <div className="flex flex-col w-full items-center p-[24px] gap-[10px]">
+        <div className="flex items-end w-full h-[155px] rounded-[20px] py-[10px] px-[16px] drop-shadow-lg bg-[url('../public/food.png')] bg-cover bg-center">
+          <p className="text-white text-[28px] font-bold drop-shadow-lg leading-tight">
+            Food
+          </p>
+        </div>
+      </div>
+
+      <BottomNavBar />
     </main>
-);
+  );
 }
