0ce0e5001c3a37cb44b19565e032e2950040c833
Abdul G Zziwa <143699732+abduzziwa@users.noreply.github.com>
2024-09-18 10:15:16 +0200

    API ENDPOINTS.
    API endpoint added. /table

---
+++ app/api/models/tables.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const tableSchema = new mongoose.Schema({
+  tableNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  occupied: {
+    type: Boolean,
+    default: false,
+  },
+  currentOrders: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Order',
+  },
+});
+
+export const Table =  mongoose.model('Table', tableSchema, 'tables');
