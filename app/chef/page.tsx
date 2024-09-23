d9e976917f904cfaddfa9fd6354e33775fccd26e
Abdul G Zziwa <143699732+abduzziwa@users.noreply.github.com>
2024-09-23 12:28:05 +0200

    Waiter Chef and database 90%.

--- app/chef/page.tsx
+++ app/chef/page.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import OrdersManager from "./components/OrdersManager";
 import styles from "./components/chef.module.css";
 
 const Page = () => {
+  const [refreshKey, setRefreshKey] = useState(0); // Key to force re-mount
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRefreshKey((prevKey) => prevKey + 1); // Trigger a re-render by updating the key
+    }, 2000); // Refresh every 2 seconds
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
-      <main className={`${styles.mainChef} w-full p-4`}>
+      <main key={refreshKey} className={`${styles.mainChef} mt-4 m-[3.5rem]`}>
         <OrdersManager />
       </main>
     </>
