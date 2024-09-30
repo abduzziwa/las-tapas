c4d2dbebe41869e1a68ec4d1fb4494deab7a913b
m3lliflu0us <bryce.van.der.werf@student.gildeopleidingen.nl>
2024-09-30 09:45:05 +0200

    update loginSystem

---
+++ app/api/models/employees.ts
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  employeeId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  shiftDetails: {
+    type: String,
+    required: true
+  }
+});
+
+export const Employees = mongoose.models.Employees || mongoose.model('Employees', employeeSchema, 'employees');
+
