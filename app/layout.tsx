62f7bb7eb1178eb6d8faac5a0446ea01dc1ce84b
m3lliflu0us <bryce.van.der.werf@student.gildeopleidingen.nl>
2024-09-30 09:50:37 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Comfortaa } from "@next/font/google";
+import { Comfortaa } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Las Tapas",
   description: "Welcome to Las Tapas! Explore our delicious menu, place your order for dine-in, takeaway, or delivery, and track your order in real-time. Enjoy a seamless dining experience with us!",
 };
 
-const confortaa = Comfortaa({
+const comfortaa = Comfortaa({
   subsets: ["latin"],
-})
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ confortaa.className }>{children}</body>
+      <body className={comfortaa.className}>{children}</body>
     </html>
   );
 }
